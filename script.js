463af4f2944e05b91f7466f3259ccc48b34fd3bb
Brennda Landim <125628894+brenndalandim@users.noreply.github.com>
2024-03-12 11:18:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+escolhaUsuario = prompt("★ Bem Vindo ao jogo da sorte! ★ \nEscolha par ou ímpar:")
+numSorteado = Math.floor(Math.random() * 100)
+
+if (numSorteado % 2 === 0) {
+  numResultado = "par"
+} else {
+  numResultado = "ímpar"
+}
+
+if (escolhaUsuario != "par" && escolhaUsuario != "ímpar") {
+  alert("Você não escolheu par ou ímpar \n==========\n Você Perdeu!\n==========")
+} else {
+  if (escolhaUsuario === "par" && numResultado === "par") {
+    alert(`O número sorteado foi ${numSorteado} \n==========\n Você Ganhou!\n==========`)
+  } else if (escolhaUsuario === "ímpar" && numResultado === "ímpar") {
+    alert(`O número sorteado foi ${numSorteado} \n==========\n Você Ganhou!\n==========`)
+  } else {
+      alert(`O número sorteado foi ${numSorteado} \n==========\n Você Perdeu!\n==========`)
+  }
+}
